60e2a8c12553c25cb9d453278d8f6088463361c7
libertininick <libertini.nick@gmail.com>
2025-04-07 19:50:54 -0600

    Add PCA model

---
+++ src/imagescry/decomposition.py
@@ -0,0 +1,165 @@
+"""Matrix decomposition algorithms."""
+
+import torch
+from jaxtyping import Float, jaxtyped
+from lightning import LightningModule
+from torch import Tensor, nn
+
+from imagescry.typechecking import typechecker
+
+
+class PCA(LightningModule):
+    """Principal component analysis (PCA).
+
+    Linear dimensionality reduction using Singular Value Decomposition of the data to project it to a lower dimensional
+    space.
+    """
+
+    def __init__(
+        self, *, min_num_components: int = 1, max_num_components: int | None = None, min_explained_variance: float = 0.0
+    ) -> None:
+        """Initialize the PCA model.
+
+        Args:
+            min_num_components (int, optional): The minimum number of principal components to keep. Defaults to 1.
+            max_num_components (int | None, optional): The maximum number of principal components to keep. If not
+                provided, the number of principal components will be determined by the minimum number of components that
+                explain at least `min_explained_variance` of the total variance. Defaults to None.
+            min_explained_variance (float, optional): The minimum percentage of variance that the PCA should explain.
+                If `max_num_components` is not provided, this will be used to determine the maximum number of
+                components. Defaults to 0.0.
+        """
+        super().__init__()
+
+        # Validate arguments
+        if min_num_components < 1:
+            raise ValueError(f"min_num_components must be at least 1, got {min_num_components}")
+        if max_num_components is not None and max_num_components < min_num_components:
+            raise ValueError(f"max_num_components must be at least {min_num_components}, got {max_num_components}")
+        if min_explained_variance < 0.0 or min_explained_variance > 1.0:
+            raise ValueError(f"min_explained_variance must be between 0.0 and 1.0, got {min_explained_variance}")
+
+        # PCA parameters
+        self.min_num_components = min_num_components
+        self.max_num_components = max_num_components
+        self.min_explained_variance = min_explained_variance
+        self.save_hyperparameters({
+            "min_num_components": min_num_components,
+            "max_num_components": max_num_components,
+            "min_explained_variance": min_explained_variance,
+        })
+
+        # Define parameters to fit
+        self._num_features: nn.Parameter
+        self._num_components: nn.Parameter
+        self.feature_means: nn.Parameter
+        self.explained_variance: nn.Parameter
+        self.component_vectors: nn.Parameter
+
+    def __repr__(self) -> str:
+        """str: Representation of the PCA model."""
+        num_features = self.num_features if self.fitted else "not fitted"
+        num_components = self.num_components if self.fitted else "not fitted"
+        return f"{self.__class__.__name__}(num_features={num_features}, num_components={num_components})"
+
+    @jaxtyped(typechecker=typechecker)
+    def forward(
+        self, x: Float[Tensor, "num_samples {self.num_features}"]
+    ) -> Float[Tensor, "num_samples {self.num_components}"]:
+        """Project the input data to a lower dimensional space.
+
+        Args:
+            x (Float[Tensor, 'num_samples {self.num_features}']): Input data.
+
+        Returns:
+            Float[Tensor, 'num_samples {self.num_components}']: Projected data.
+        """
+        return torch.matmul(x - self.feature_means, self.component_vectors)
+
+    @jaxtyped(typechecker=typechecker)
+    def fit(self, x: Float[Tensor, "num_samples num_features"]) -> "PCA":
+        """Fit the PCA model to the input data.
+
+        The input data is centered but not scaled for each feature before applying the SVD.
+        See [torch.pca_lowrank](https://pytorch.org/docs/stable/generated/torch.pca_lowrank.html) for more details.
+
+        Args:
+            x (Float[Tensor, 'num_samples num_features']): Input data with shape (num_samples, num_features).
+
+        Returns:
+            PCA: The fitted PCA model.
+
+        Raises:
+            ValueError: If the number of samples is less than 2.
+        """
+        # Validate input data
+        num_samples, num_features = x.shape
+        if num_samples < 2:
+            raise ValueError(f"num_samples must be at least 2, got {num_samples}")  # pragma: no cover
+
+        # Calculate the mean of the input data
+        self._num_features = nn.Parameter(torch.tensor(num_features), requires_grad=False)
+        self.feature_means = nn.Parameter(x.mean(dim=0, keepdim=True), requires_grad=False)
+
+        # Center the input data
+        x_centered = x - self.feature_means
+
+        # Perform singular value decomposition of centered matrix
+        _, s, v = torch.pca_lowrank(x_centered, center=False)
+
+        # Calculate explained variance ratio
+        eigenvalues = s**2 / (num_samples - 1)
+        total_variance = torch.sum(eigenvalues)
+        self.explained_variance = nn.Parameter(eigenvalues / total_variance, requires_grad=False)
+        cumulative_explained_variance = torch.cumsum(self.explained_variance, dim=0)
+
+        # Determine the number of components to keep
+        num_components_to_meet_min_explained_variance = int(
+            torch.sum(cumulative_explained_variance < self.min_explained_variance).item() + 1
+        )
+        num_components = max(self.min_num_components, num_components_to_meet_min_explained_variance)
+        if self.max_num_components is not None:
+            num_components = min(self.max_num_components, num_components)
+        self._num_components = nn.Parameter(torch.tensor(num_components), requires_grad=False)
+
+        # Select components
+        self.component_vectors = nn.Parameter(v[:, :num_components], requires_grad=False)
+
+        return self
+
+    @jaxtyped(typechecker=typechecker)
+    def transform(self, x: Float[Tensor, "num_samples num_features"]) -> Float[Tensor, "num_samples num_components"]:
+        """Project the input data to a lower dimensional space.
+
+        Args:
+            x (Float[Tensor, 'num_samples {self.num_features}']): Input data.
+
+        Returns:
+            Float[Tensor, 'num_samples {self.num_components}']: Projected data.
+
+        Raises:
+            RuntimeError: If the PCA model is not fitted.
+        """
+        if not self.fitted:
+            raise RuntimeError("PCA model not fitted")  # pragma: no cover
+        return self(x)
+
+    @property
+    def fitted(self) -> bool:
+        """bool: Whether the PCA model has been fitted."""
+        try:
+            # Check that the number of components is between 1 and the number of features
+            return 1 <= self.num_components <= self.num_features
+        except AttributeError:
+            # If the number of components or features is not set, the model is not fitted
+            return False
+
+    @property
+    def num_features(self) -> int:
+        """int: Number of features in the input data."""
+        return int(self._num_features.item())
+
+    @property
+    def num_components(self) -> int:
+        """int: Number of principal components."""
+        return int(self._num_components.item())
---
+++ tests/test_decomposition.py
@@ -0,0 +1,119 @@
+"""Tests for the decomposition module."""
+
+import pytest
+import torch
+from jaxtyping import Float
+from pytest_check import check
+from torch import Tensor
+from torch.distributions import MultivariateNormal
+
+from imagescry.decomposition import PCA
+
+# Fixtures
+num_samples = 1_000
+feature_locs = torch.tensor([0.0, 1.0, -1.0, 0.0])
+num_features = feature_locs.shape[0]
+
+
+@pytest.fixture(scope="module")
+def uncorrelated_features() -> Float[Tensor, f"{num_samples} {num_features}"]:
+    """Fixture generating uncorrelated features."""
+    torch.manual_seed(1234)
+    dist = MultivariateNormal(loc=feature_locs, covariance_matrix=torch.eye(num_features))
+    return dist.sample((num_samples,))
+
+
+@pytest.fixture(scope="module")
+def correlated_features() -> Float[Tensor, f"{num_samples} {num_features}"]:
+    """Fixture generating correlated features."""
+    torch.manual_seed(1234)
+    dist = MultivariateNormal(
+        loc=feature_locs,
+        covariance_matrix=torch.tensor([
+            [1.0, 0.5, 0.0, 0.0],
+            [0.5, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, -0.5],
+            [0.0, 0.0, -0.5, 1.0],
+        ]),
+    )
+    return dist.sample((num_samples,))
+
+
+@pytest.mark.parametrize("min_explained_variance, expected_num_components", [(0.2, 1), (0.4, 2), (0.6, 3), (1.0, 4)])
+def test_pca_uncorrelated_features(
+    uncorrelated_features: Float[Tensor, f"{num_samples} {num_features}"],
+    min_explained_variance: float,
+    expected_num_components: int,
+) -> None:
+    """Test principal component decomposition on uncorrelated features."""
+    # Initialize PCA model
+    pca = PCA(min_explained_variance=min_explained_variance)
+
+    # Check that the PCA object is not fitted before fitting
+    check.is_false(pca.fitted)
+
+    # Fit PCA model
+    pca.fit(uncorrelated_features)
+
+    # Check that the PCA object is fitted after fitting
+    check.is_true(pca.fitted)
+
+    # Check length of feature means and explained variances
+    check.equal(pca.feature_means.data.size(1), num_features)
+    check.equal(len(pca.explained_variance.data), num_features)
+
+    # Project features
+    projected_features = pca.transform(uncorrelated_features)
+
+    # Check that the number of projected components is as expected
+    check.equal((num_samples, expected_num_components), projected_features.shape)
+
+    # Check explained variance is as expected
+    check.greater_equal(pca.explained_variance.data[:expected_num_components].sum().item(), min_explained_variance)
+
+    # Check correlation of projected features are close to zero
+    if expected_num_components > 1:
+        atol = 1e-4
+        corr_matrix_abs = torch.abs(torch.corrcoef(projected_features.T))
+        check.is_true(torch.all(torch.tril(corr_matrix_abs, diagonal=-1) <= atol))
+
+
+@pytest.mark.parametrize(
+    "min_explained_variance, expected_num_components", [(0.2, 1), (0.4, 2), (0.6, 2), (0.8, 3), (1.0, 4)]
+)
+def test_pca_correlated_features(
+    correlated_features: Float[Tensor, f"{num_samples} {num_features}"],
+    min_explained_variance: float,
+    expected_num_components: int,
+) -> None:
+    """Test principal component decomposition on correlated features."""
+    # Initialize PCA model
+    pca = PCA(min_explained_variance=min_explained_variance)
+
+    # Check that the PCA object is not fitted before fitting
+    check.is_false(pca.fitted)
+
+    # Fit PCA model
+    pca.fit(correlated_features)
+
+    # Check that the PCA object is fitted after fitting
+    check.is_true(pca.fitted)
+
+    # Check length of feature means and explained variances
+    check.equal(pca.feature_means.data.size(1), num_features)
+    check.equal(len(pca.explained_variance.data), num_features)
+
+    # Project features
+    projected_features = pca.transform(correlated_features)
+
+    # Check that the number of projected components is as expected
+    check.equal((num_samples, expected_num_components), projected_features.shape)
+
+    # Check explained variance is as expected
+    check.greater_equal(pca.explained_variance.data[:expected_num_components].sum().item(), min_explained_variance)
+
+    # Check correlation of projected features are close to zero
+    if expected_num_components > 1:
+        atol = 1e-4
+        corr_matrix_abs = torch.abs(torch.corrcoef(projected_features.T))
+        check.is_true(torch.all(torch.tril(corr_matrix_abs, diagonal=-1) <= atol))
