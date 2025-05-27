959d412533c1b0aea6a941c036b3b9f1e8c4e69e
libertininick <libertini.nick@gmail.com>
2025-05-26 19:49:02 -0600

    Add preprocess step to AbstractEmbeddingModel
    This step is where resizing and normalization should be implemented

--- src/imagescry/embedding.py
+++ src/imagescry/embedding.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from typing import Literal
 
 import torch
-from jaxtyping import Float, Int64, Num, jaxtyped
+from jaxtyping import Float, Int64, UInt8, jaxtyped
 from lightning import LightningModule
 from torch import Tensor, nn
 from torchvision.models import (
@@ -18,7 +18,7 @@ from torchvision.models import (
 )
 
 from imagescry.image.dataset import ImageBatch
-from imagescry.image.transforms import normalize_per_channel
+from imagescry.image.transforms import normalize_per_channel, resize
 from imagescry.typechecking import typechecker
 
 
@@ -47,19 +47,24 @@ class EmbeddingBatch:
                 f"Got indices on {self.indices.device} and embeddings on {self.embeddings.device}"
             )
 
-    def to(self, device: torch.device) -> "EmbeddingBatch":
+    def cpu(self) -> "EmbeddingBatch":
+        """Move tensors to the CPU.
+
+        Returns:
+            EmbeddingBatch: A new EmbeddingBatch with tensors on the CPU.
+        """
+        return self.to("cpu")
+
+    def to(self, device: str | torch.device) -> "EmbeddingBatch":
         """Move tensors to the specified device.
 
         Args:
-            device (torch.device): The device to move the tensors to.
+            device (str | torch.device): The device to move the tensors to.
 
         Returns:
             EmbeddingBatch: A new EmbeddingBatch with tensors on the specified device.
         """
-        return EmbeddingBatch(
-            indices=self.indices.to(device),
-            embeddings=self.embeddings.to(device),
-        )
+        return EmbeddingBatch(indices=self.indices.to(device), embeddings=self.embeddings.to(device))
 
     @property
     def device(self) -> torch.device:
@@ -73,19 +78,33 @@ class AbstractEmbeddingModel(ABC, LightningModule):
     This class defines the interface for all embedding models. It inherits from `LightningModule` and implements the
     `predict_step` method to standardize the prediction interface for all embedding models.
 
-    Subclasses must implement the `__init__` and `forward` methods and define the `embedding_dim` and
-    `downsample_factor` properties. The `__init__` method should save required initialization parameters using
-    `self.save_hyperparameters()`. The `forward` method should handle any preprocessing steps required for the model.
+    Subclasses must implement the `__init__`, `preprocess` and `forward` methods and define an `embedding_dim` property.
+    - The `__init__` method should save required initialization parameters using `self.save_hyperparameters()`.
+    - The `preprocess` method should preprocess the images to the same format as the model expects.
+    - The `forward` method should extract the embedding feature maps from the images.
+    - The `predict_step` will call the `preprocess` and `forward` methods in sequence and return an `EmbeddingBatch`.
     """
 
     @abstractmethod
-    def forward(self, x: Num[Tensor, "B C H1 W1"]) -> Float[Tensor, "B E H2 W2"]:
+    def preprocess(self, images: UInt8[Tensor, "B C H1 W1"]) -> Float[Tensor, "B C H2 W2"]:
+        """Preprocess images to the same format as the model expects.
+
+        Args:
+            images (UInt8[Tensor, "B C H1 W1"]): Batch of images.
+
+        Returns:
+            Float[Tensor, "B C H2 W2"]: Preprocessed images.
+        """
+        ...  # pragma: no cover
+
+    @abstractmethod
+    def forward(self, x: Float[Tensor, "B C H1 W1"]) -> Float[Tensor, "B E H2 W2"]:
         """Forward pass."""
         ...  # pragma: no cover
 
     @jaxtyped(typechecker=typechecker)
     def predict_step(self, batch: ImageBatch) -> EmbeddingBatch:
-        """Extract embedding feature maps and L2 normalizes each embedding vector for a batch of images.
+        """Preprocess images, extract embedding feature maps and L2 normalizes each embedding vector for an ImageBatch.
 
         Args:
             batch (ImageBatch): Batch of images and their dataset indices.
@@ -93,19 +112,16 @@ class AbstractEmbeddingModel(ABC, LightningModule):
         Returns:
             EmbeddingBatch: Embedding feature map for the batch of images.
         """
+        # Preprocess images
+        x = self.preprocess(batch.images)
+
         # Extract embedding feature map
-        x = self.forward(batch.images)
+        x = self.forward(x)
 
         # L2 normalize embedding
         x = nn.functional.normalize(x, p=2, dim=1)
 
         return EmbeddingBatch(indices=batch.indices, embeddings=x)
-
-    @property
-    @abstractmethod
-    def downsample_factor(self) -> int:
-        """int: Spatial downsample factor."""
-        ...  # pragma: no cover
 
     @property
     @abstractmethod
@@ -117,11 +133,14 @@ class AbstractEmbeddingModel(ABC, LightningModule):
 class EfficientNetEmbedder(AbstractEmbeddingModel):
     """Embedding model using EfficientNetV2 as the backbone feature extractor."""
 
-    def __init__(self, *, backbone_size: Literal["s", "m", "l"] = "s", pretrained: bool = False) -> None:
+    def __init__(
+        self, *, backbone_size: Literal["s", "m", "l"] = "s", max_side_length: int = 640, pretrained: bool = False
+    ) -> None:
         """Initialize the model.
 
         Args:
             backbone_size (Literal["s", "m", "l"]): The size of the backbone model to load. Defaults to "s".
+            max_side_length (int): The maximum side length of the images to resize to. Defaults to 640.
             pretrained (bool): Whether to load pretrained weights. Defaults to False.
 
         Raises:
@@ -130,11 +149,11 @@ class EfficientNetEmbedder(AbstractEmbeddingModel):
         super().__init__()
 
         self._embedding_dim = 1_280
-        self._downsample_factor = 32
 
         # Save initialization parameters
-        self.save_hyperparameters({"backbone_size": backbone_size})
+        self.save_hyperparameters({"backbone_size": backbone_size, "max_side_length": max_side_length})
         self.backbone_size = backbone_size
+        self.max_side_length = max_side_length
 
         # Set architecture & weights
         if self.backbone_size == "s":
@@ -153,27 +172,35 @@ class EfficientNetEmbedder(AbstractEmbeddingModel):
         self.feature_layers = efficientnet_v2(weights=weights).features
 
     @jaxtyped(typechecker=typechecker)
-    def forward(self, x: Num[Tensor, "B C H1 W1"]) -> Float[Tensor, "B E H2 W2"]:
+    def preprocess(self, images: UInt8[Tensor, "B C H1 W1"]) -> Float[Tensor, "B C H2 W2"]:
+        """Resize and normalize images prior to extracting embedding feature maps.
+
+        Args:
+            images (UInt8[Tensor, "B C H1 W1"]): Batch of images.
+
+        Returns:
+            Float[Tensor, "B C H2 W2"]: Preprocessed images.
+        """
+        # Resize images so that the long side is at most `max_side_length`
+        h, w = images.shape[-2:]
+        if max(h, w) > self.max_side_length:
+            print(images.dtype)
+            images = resize(images, output_size=self.max_side_length, side_ref="long")
+
+        # Normalize pixel values to [-3, 3]
+        return normalize_per_channel(images, min_value=-3, max_value=3)
+
+    @jaxtyped(typechecker=typechecker)
+    def forward(self, x: Float[Tensor, "B C H1 W1"]) -> Float[Tensor, "B E H2 W2"]:
         """Extract embedding feature map.
 
         Args:
-            x (Num[Tensor, 'B C H1 W1']): Image tensor.
+            x (Float[Tensor, 'B C H1 W1']): Image tensor.
 
         Returns:
             Float[Tensor, 'B E H2 W2']: Image embedding feature map.
         """
-        # Normalize pixel values to [-3, 3]
-        x = normalize_per_channel(x, min_value=-3, max_value=3)
-
-        # Extract features
-        x = self.feature_layers.forward(x)
-
-        return x
-
-    @property
-    def downsample_factor(self) -> int:
-        """int: Spatial downsample factor."""
-        return self._downsample_factor
+        return self.feature_layers.forward(x)
 
     @property
     def embedding_dim(self) -> int:
