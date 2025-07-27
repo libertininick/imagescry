3f1dddfab3649e3ace941d6fb5e490949543dea1
Nick Libertini <libertini.nick@gmail.com>
2025-07-27 16:40:20 -0600

    Add tests for creating and adding items to database

---
+++ tests/test_storage/test_database.py
@@ -0,0 +1,127 @@
+"""Tests for database manager."""
+
+from collections.abc import Generator
+from pathlib import Path
+
+import pytest
+import torch
+from pytest_check import check_functions
+
+from imagescry.image.info import ImageInfo, ImageShape
+from imagescry.storage.database import DatabaseManager
+from imagescry.storage.models import Embedding
+from tests.test_storage.test_models import assert_embeddings_equal
+
+
+# Fixtures
+@pytest.fixture(scope="session")
+def embedding1() -> Embedding:
+    """Fixture to create a sample Embedding instance."""
+    # Create a sample ImageInfo
+    image_info = ImageInfo(
+        source=Path("/path/to/image-1.jpg"),
+        shape=ImageShape(width=800, height=800),
+        md5_hash="1",
+    )
+
+    # Create a sample embedding tensor
+    embedding_tensor = torch.randn(128, 20, 20)
+
+    # Create an Embedding instance from the ImageInfo and tensor
+    return Embedding.create(image_info=image_info, embedding_tensor=embedding_tensor, pca_checkpoint_id=1)
+
+
+@pytest.fixture(scope="session")
+def embedding2() -> Embedding:
+    """Fixture to create a sample Embedding instance."""
+    # Create a sample ImageInfo
+    image_info = ImageInfo(
+        source=Path("/path/to/image-2.jpg"),
+        shape=ImageShape(width=800, height=600),
+        md5_hash="2",
+    )
+
+    # Create a sample embedding tensor
+    embedding_tensor = torch.randn(128, 20, 15)
+
+    # Create an Embedding instance from the ImageInfo and tensor
+    return Embedding.create(image_info=image_info, embedding_tensor=embedding_tensor, pca_checkpoint_id=1)
+
+
+@pytest.fixture(scope="session")
+def embedding3() -> Embedding:
+    """Fixture to create a sample Embedding instance."""
+    # Create a sample ImageInfo
+    image_info = ImageInfo(
+        source=Path("/path/to/image-3.jpg"),
+        shape=ImageShape(width=600, height=800),
+        md5_hash="3",
+    )
+
+    # Create a sample embedding tensor
+    embedding_tensor = torch.randn(128, 15, 20)
+
+    # Create an Embedding instance from the ImageInfo and tensor
+    return Embedding.create(image_info=image_info, embedding_tensor=embedding_tensor, pca_checkpoint_id=1)
+
+
+@pytest.fixture(scope="session")
+def db_manager(tmp_path_factory: pytest.TempPathFactory) -> Generator[DatabaseManager, None, None]:
+    """Fixture to create a DatabaseManager instance."""
+    with DatabaseManager(db_dir=tmp_path_factory.mktemp("db_dir")) as db_manager:
+        yield db_manager
+
+
+# Tests
+def test_create_delete_database(tmp_path: Path) -> None:
+    """Test creating and deleting a database."""
+    # Create the database by initializing DatabaseManager
+    db_manager = DatabaseManager(db_dir=tmp_path)
+    check_functions.is_true(db_manager.database_exists, "Database file should be created at the specified path.")
+    check_functions.is_true(db_manager.is_connected, "Database manager should be connected after creation.")
+
+    # Delete the database
+    check_functions.is_true(db_manager.delete_database(), "Database should be deleted successfully.")
+    check_functions.is_false(db_manager.is_connected, "Database manager should be disconnected after deletion.")
+    check_functions.is_false(db_manager.database_exists, "Database file should not exist after deletion.")
+
+
+def test_add_and_get_embedding(db_manager: DatabaseManager, embedding1: Embedding) -> None:
+    """Test adding and retrieving an embedding."""
+    # Clone the embedding
+    embedding1_clone = embedding1.model_copy(deep=True)
+
+    # Add the embedding to the database
+    item_id = db_manager.add_item(embedding1)
+
+    # Retrieve the item by ID
+    retrieved_item = db_manager.get_item(Embedding, item_id)
+    if retrieved_item is None:
+        pytest.fail("Retrieved item is None, expected an Embedding instance.")
+
+    # Check that the retrieved item matches the original
+    assert_embeddings_equal(retrieved_item, embedding1)
+
+    # Test trying to add the same item again raises an error because of unique hash/filepath constraints
+    with pytest.raises(RuntimeError):
+        db_manager.add_item(embedding1_clone)
+
+
+def test_add_and_get_multiple_embeddings(
+    db_manager: DatabaseManager, embedding2: Embedding, embedding3: Embedding
+) -> None:
+    """Test adding and retrieving multiple embeddings."""
+    # Add multiple embeddings to the database
+    item_ids = db_manager.add_items([embedding2, embedding3])
+    check_functions.equal([2, 3], sorted(item_ids), "Item IDs do not match expected values")
+
+    # Retrieve the items by IDs
+    retrieved_items = db_manager.get_items(Embedding, item_ids)
+    assert len(retrieved_items) == 2
+
+    # Index the items by ID for easy comparison
+    retrieved_items = {item.id: item for item in retrieved_items}
+
+    # Check that the retrieved items match the originals
+    assert_embeddings_equal(retrieved_items[2], embedding2)
+    assert_embeddings_equal(retrieved_items[3], embedding3)
